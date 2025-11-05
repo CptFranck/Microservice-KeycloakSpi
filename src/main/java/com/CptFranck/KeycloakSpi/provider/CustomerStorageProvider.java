ff0c222a92c65bc7ee536cf4db9da35d91400dcd
CptFranck <francois.caillet71@gmail.com>
2025-11-05 16:25:30 +0100

    add CustomerStorageProvider to keycloak spi

---
+++ src/main/java/com/CptFranck/KeycloakSpi/provider/CustomerStorageProvider.java
@@ -0,0 +1,42 @@
+//package com.CptFranck.CustomerService.provider;
+//
+//import org.keycloak.models.RealmModel;
+//import org.keycloak.models.UserModel;
+//import org.keycloak.storage.UserStorageProvider;
+//import org.keycloak.storage.user.UserLookupProvider;
+//import org.keycloak.storage.user.UserRegistrationProvider;
+//
+//
+//public class CustomerStorageProvider implements UserStorageProvider,
+//                                                UserRegistrationProvider,
+//                                                UserLookupProvider {
+//
+//    @Override
+//    public void close() {
+//    }
+//
+//    @Override
+//    public UserModel addUser(RealmModel realmModel, String s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean removeUser(RealmModel realmModel, UserModel userModel) {
+//        return false;
+//    }
+//
+//    @Override
+//    public UserModel getUserById(RealmModel realmModel, String s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public UserModel getUserByUsername(RealmModel realmModel, String s) {
+//        return null;
+//    }
+//
+//    @Override
+//    public UserModel getUserByEmail(RealmModel realmModel, String s) {
+//        return null;
+//    }
+//}
