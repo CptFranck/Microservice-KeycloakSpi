46c7f130b236e641c9204d4445ec95f460ea5ad0
CptFranck <francois.caillet71@gmail.com>
2025-11-05 11:54:11 +0100

    update CustomerServiceClient debug with log

--- src/main/java/com/CptFranck/KeycloakSpi/client/CustomerServiceClient.java
+++ src/main/java/com/CptFranck/KeycloakSpi/client/CustomerServiceClient.java
@@ -1,12 +1,15 @@
 package com.CptFranck.KeycloakSpi.client;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 public class CustomerServiceClient {
 
     private final String customerServiceUrl;
@@ -18,15 +21,19 @@ public class CustomerServiceClient {
     }
 
     public void createCustomerFromKeycloak(String id, String username, String email) {
+        log.info("Create customer from keycloak");
         String json = customerToJson(id, username, email);
         sendAsyncRequest("/customers", "POST", json);
     }
 
     public void updateCustomer(String id, String username, String email) {
+        log.info("Update customer from keycloak");
         String json = customerToJson(id, username, email);
-        sendAsyncRequest("/customers/" + id, "PUT", json);    }
+        sendAsyncRequest("/customers/" + id, "PUT", json);
+    }
 
     public void deleteCustomer(String userId) {
+        log.info("Delete customer from keycloak");
         sendAsyncRequest("/customers/" + userId, "DELETE", null);
     }
 
@@ -42,6 +49,8 @@ public class CustomerServiceClient {
                 .uri(URI.create(customerServiceUrl + path))
                 .header("Content-Type", "application/json");
 
+        log.info("Sending request to {}{}", customerServiceUrl, path);
+
         if (body != null)
             builder.method(method, HttpRequest.BodyPublishers.ofString(body, StandardCharsets.UTF_8));
         else
@@ -50,9 +59,9 @@ public class CustomerServiceClient {
         httpClient.sendAsync(builder.build(), HttpResponse.BodyHandlers.ofString())
                 .whenComplete((response, throwable) -> {
                     if (throwable != null)
-                        System.err.println("Error calling customer service: " + throwable.getMessage());
+                        log.error("Error calling customer service: {}", throwable.getMessage());
                     else
-                        System.out.println("CustomerService [" + method + " " + path + "] → " + response.statusCode());
+                        log.info("Successfully called customer service : {}", response.statusCode());
                 });
     }
 }
