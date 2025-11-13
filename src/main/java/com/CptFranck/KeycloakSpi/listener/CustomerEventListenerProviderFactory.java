bcb5d81a209882ff9fb22466eb57bd29d0fd5e75
CptFranck <francois.caillet71@gmail.com>
2025-11-13 17:23:07 +0100

    update CustomerEventListenerProviderFactory

--- src/main/java/com/CptFranck/KeycloakSpi/listener/CustomerEventListenerProviderFactory.java
+++ src/main/java/com/CptFranck/KeycloakSpi/listener/CustomerEventListenerProviderFactory.java
@@ -9,16 +9,22 @@ import org.keycloak.models.KeycloakSessionFactory;
 
 public class CustomerEventListenerProviderFactory implements EventListenerProviderFactory {
 
+    private String jwtUri;
+    private String clientId;
+    private String clientSecret;
     private String customerServiceUrl;
 
     @Override
     public EventListenerProvider create(KeycloakSession keycloakSession) {
-        CustomerServiceClient client = new CustomerServiceClient(customerServiceUrl);
+        CustomerServiceClient client = new CustomerServiceClient(jwtUri, clientId, clientSecret, customerServiceUrl);
         return new CustomerEventListenerProvider(keycloakSession, client);
     }
 
     @Override
     public void init(Config.Scope scope) {
+        this.jwtUri = System.getenv().getOrDefault("JWT_URI", "http://localhost:8091/realms/ticketing-security-realm");
+        this.clientId = System.getenv().getOrDefault("JWT_CLIENT_ID", "event-listener-client");
+        this.clientSecret = System.getenv().getOrDefault("JWT_CLIENT_SECRET", "client-secret");
         this.customerServiceUrl = System.getenv().getOrDefault("CUSTOMER_SERVICE_API_URL", "http://localhost:8086/api/v1");
     }
 
