e599739b8313b6b5963b9329a8c85832bd6f0522
CptFranck <francois.caillet71@gmail.com>
2025-11-13 17:22:35 +0100

    update CustomerServiceClient

--- src/main/java/com/CptFranck/KeycloakSpi/client/CustomerServiceClient.java
+++ src/main/java/com/CptFranck/KeycloakSpi/client/CustomerServiceClient.java
@@ -4,8 +4,13 @@ package com.CptFranck.KeycloakSpi.client;
 import com.CptFranck.dto.KeycloakUserDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
 import org.keycloak.models.UserModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,12 +19,19 @@ import java.nio.charset.StandardCharsets;
 
 public class CustomerServiceClient {
 
+    private static final Logger log = LoggerFactory.getLogger(CustomerServiceClient.class);
+    private final String jwtUri;
+    private final String clientId;
+    private final String clientSecret;
     private final String customerServiceUrl;
     private final HttpClient httpClient;
 
-    public CustomerServiceClient(String url) {
-        this.httpClient = HttpClient.newHttpClient();
+    public CustomerServiceClient(String tokenUrl, String clientId, String clientSecret, String url) {
+        this.jwtUri = tokenUrl;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
         this.customerServiceUrl = url;
+        this.httpClient = HttpClient.newHttpClient();
     }
 
     public void createCustomerFromKeycloak(UserModel user) {
@@ -56,18 +68,45 @@ public class CustomerServiceClient {
 
     private void sendAsyncRequest(String path, String method, String body) {
         try {
+            log.info("Sending {} request to {} with {}", method, customerServiceUrl + path, body);
+
+            String token = getAccessToken();
             HttpRequest.Builder builder = HttpRequest.newBuilder()
                 .uri(URI.create(customerServiceUrl + path))
-                .header("Content-Type", "application/json");
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + token);
 
             if (body != null)
                 builder.method(method, HttpRequest.BodyPublishers.ofString(body, StandardCharsets.UTF_8));
             else
                 builder.method(method, HttpRequest.BodyPublishers.noBody());
 
-            httpClient.send(builder.build(), HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = httpClient.send(builder.build(), HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200)
+                log.error("Failed : HTTP error code : {}", response.statusCode());
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private String getAccessToken() throws Exception {
+        String requestBody = String.format(
+                "grant_type=client_credentials&client_id=%s&client_secret=%s",
+                clientId, clientSecret
+        );
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(jwtUri))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200)
+            throw new RuntimeException("Failed to get access token: " + response.body());
+
+        JsonObject json = Json.createReader(new StringReader(response.body())).readObject();
+        return json.getString("access_token");
     }
 }
